31fcacd78a6d849b8f175132567b97fae0b1fb15
Caio Labesten <caiovlf@gmail.com>
2020-06-15 15:15:07 -0300

    Initial Commit

---
+++ ecommerce/src/main/java/com/caiovlf/ecommerce/EmailService.java
@@ -0,0 +1,32 @@
+package com.caiovlf.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+public class EmailService {
+
+    public static void main(String[] args) {
+        var emailService = new EmailService();
+        try(var service = new KafkaService(EmailService.class.getSimpleName(),
+                "ECOMMERCE_SEND_EMAIL",
+                emailService::parse)){
+            service.run();
+        }
+
+    }
+    private void parse(ConsumerRecord<String, String> record) {
+        System.out.println("--------------------------------------");
+        System.out.println("Sending e-mail!");
+        System.out.println("Key: " + record.key());
+        System.out.println("Value: " + record.value());
+        System.out.println("Partition: " + record.partition());
+        System.out.println("OffSet: " + record.offset());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            //ignoring
+            e.printStackTrace();
+        }
+        System.out.println("Order has been processed");
+    }
+
+}
